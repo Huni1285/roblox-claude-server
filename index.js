4aaadfbfd53e2f56e0e441c6cf234396ee546ef7
Huni <huni.poltl@yahoo.com>
2026-06-06 23:21:53 +0100

    add health check

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
 app.post('/chat', async (req, res) => {
   const { message } = req.body;
   try {
