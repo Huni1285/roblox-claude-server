8aec769eeb1363cfdb8ffa0683080ee788f43954
Huni <huni.poltl@yahoo.com>
2026-06-06 23:40:43 +0100

    fix port

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ app.post('/chat', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
